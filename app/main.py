d8199cb19229757ebdc6b7199173628ad8bdf39f
CongLuanTran <congluan.tran19@gmail.com>
2025-04-29 03:19:28 +0700

    concurrent connection

--- app/main.py
+++ app/main.py
@@ -16,25 +16,34 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, address = server_socket.accept()  # wait for client
-    request = conn.recv(4096).decode()
-    header, body = request.split("\r\n\r\n")
-    header = header.split("\r\n")
-    method, path, version = header[0].split()
-    header_fields = {
-        k.strip(): v.strip()
-        for k, v in map(lambda line: line.split(":", 1), header[1:])
-    }
+    print("Server is running at port 4221")
 
-    if path == "/":
-        conn.sendall(http_200_ok(""))
-    elif path.startswith("/echo/"):
-        target = path.split("/")[2]
-        conn.sendall(http_200_ok(target))
-    elif path == "/user-agent":
-        conn.sendall(http_200_ok(header_fields["User-Agent"]))
-    else:
-        conn.sendall(http_404_not_found())
+    try:
+        while True:
+            conn, address = server_socket.accept()  # wait for client
+            request = conn.recv(4096).decode()
+            header, body = request.split("\r\n\r\n")
+            header = header.split("\r\n")
+            method, path, version = header[0].split()
+            header_fields = {
+                k.strip(): v.strip()
+                for k, v in map(lambda line: line.split(":", 1), header[1:])
+            }
+
+            if path == "/":
+                conn.sendall(http_200_ok(""))
+            elif path.startswith("/echo/"):
+                target = path.split("/")[2]
+                conn.sendall(http_200_ok(target))
+            elif path == "/user-agent":
+                conn.sendall(http_200_ok(header_fields["User-Agent"]))
+            else:
+                conn.sendall(http_404_not_found())
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        print("Closing connection")
+        server_socket.close()
 
 
 if __name__ == "__main__":
