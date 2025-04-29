850c19cff47e1c326ae03ef2079c1ffd61c90c77
CongLuanTran <congluan.tran19@gmail.com>
2025-04-29 12:54:38 +0700

    refactor and retry concurrency

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import socket  # noqa: F401
 
 
 def http_200_ok(message):
-    return f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(message)}\r\n\r\n{message}".encode()
+    headers = {"Content-Length": f"{len(message)}"} if message else {}
+    return HTTPResponse("HTTP/1.1", 200, "OK", headers, message)
 
 
 def http_404_not_found():
-    return b"HTTP/1.1 404 Not Found"
+    return HTTPResponse("HTTP/1.1", 404, "Not Found", {}, "")
 
 
 def main():
@@ -22,28 +23,68 @@ def main():
         while True:
             conn, address = server_socket.accept()  # wait for client
             request = conn.recv(4096).decode()
-            header, body = request.split("\r\n\r\n")
-            header = header.split("\r\n")
-            method, path, version = header[0].split()
-            header_fields = {
-                k.strip(): v.strip()
-                for k, v in map(lambda line: line.split(":", 1), header[1:])
-            }
+            request = HTTPRequest(request)
 
-            if path == "/":
-                conn.sendall(http_200_ok(""))
-            elif path.startswith("/echo/"):
-                target = path.split("/")[2]
-                conn.sendall(http_200_ok(target))
-            elif path == "/user-agent":
-                conn.sendall(http_200_ok(header_fields["User-Agent"]))
+            if request.path == "/":
+                response = http_200_ok("")
+            elif request.path.startswith("/echo/"):
+                target = request.path.split("/")[2]
+                response = http_200_ok(target)
+            elif request.path == "/user-agent":
+                response = http_200_ok(request.headers["User-Agent"])
             else:
-                conn.sendall(http_404_not_found())
+                response = http_404_not_found()
+
+            conn.sendall(bytes(response))
     except Exception as e:
         print(f"Error: {e}")
     finally:
         print("Closing connection")
         server_socket.close()
+
+
+class HTTPRequest:
+    def __init__(self, request):
+        head, self.body = request.split("\r\n\r\n")
+
+        head = head.split("\r\n")
+        self.method, self.path, self.version = head[0].split()
+        self.headers = {
+            k.strip(): v.strip()
+            for k, v in map(lambda line: line.split(":", 1), head[1:])
+        }
+
+    def __str__(self):
+        headers = "\r\n".join(f"{k}: {v}" for k, v in self.headers.items())
+        return (
+            f"{self.method} {self.path} {self.version}\r\n{headers}\r\n\r\n{self.body}"
+        )
+
+    def __bytes__(self):
+        return self.__str__().encode()
+
+
+class HTTPResponse:
+    def __init__(
+        self,
+        version: str,
+        status_code: int,
+        status_message: str,
+        headers: dict[str, str],
+        body: str,
+    ):
+        self.version = version
+        self.status_code = status_code
+        self.status_message = status_message
+        self.headers = headers
+        self.body = body
+
+    def __str__(self):
+        headers = "\r\n".join(f"{k}: {v}" for k, v in self.headers.items())
+        return f"{self.version} {self.status_code} {self.status_message}\r\n{headers}\r\n\r\n{self.body}"
+
+    def __bytes__(self):
+        return self.__str__().encode()
 
 
 if __name__ == "__main__":
